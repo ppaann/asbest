6ddffabb9586ad41dc2299f6c2242e61de6e221a
Pan Pan <pan.2.pan@microsoft.com>
2016-03-01 17:20:26 +0200

    mainmenu

---
+++ app/js/directives/mainNavigation.js
@@ -0,0 +1,12 @@
+(function () {
+
+    'use strict';
+
+    angular.module('asbest').directive('mainNavigation', function (){
+        return {
+            restrict: 'E',
+            templateUrl: 'app/js/directives/mainNavigation.html'
+        }
+
+    });
+})();
