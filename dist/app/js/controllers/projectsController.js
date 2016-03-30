e09ec8660f399f27cace6732e9f4005cbfc4fef2
pan pan <pan.2.pan@microsoft.com>
2016-03-30 09:32:09 +0300

    missing file

--- dist/app/js/controllers/projectsController.js
+++ dist/app/js/controllers/projectsController.js
@@ -2,6 +2,12 @@
 
 asbest.controller('projectsController',
   function projectsController ($scope, projectData){
-
+     $scope.topNaviItems = [{
+      'name': 'Dashboard',
+      'url': '#/'
+    },{
+      'name': 'Projects',
+      'url': ''
+    }];
     $scope.projects = projectData.getAllProjects();
 })
