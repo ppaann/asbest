4d425d92cff4d62302897f168bd7faf81108d7a9
pan pan <pan.2.pan@microsoft.com>
2016-03-31 23:27:44 +0300

    Menu

--- app/js/workspace/workspace.js
+++ app/js/workspace/workspace.js
@@ -10,14 +10,40 @@
             controller: "workspaceController",
             controllerAs: 'wsCtrl',
             link: function(scope, el, attr){
-              $("#sampleDialogContent").mmenu({
-                  offCanvas: false,
-                  navbar: {
-                    title: 'Sample'
+              $("#sampleDialogMenu")
+                .mmenu({
+                    offCanvas : false,
+                    extensions  : ["theme-white"],
+                    navbar    : {
+                      title : "Sample"
+                    },
+                    navbars   : true,
+                    onClick   : {
+                      setSelected : false
+                    }
+                }).on( 'click',
+                  'a[href^="#/"]',
+                  function() {
+                    alert( "Thank you for clicking, but that's a demo link." );
+                    return false;
                   }
+                );
+              $(function() {
+                var $settings = $("#app");
+
+                var api = $("#sampleDialogMenu").data( "mmenu" );
+
+                //  Choose location
+                var $set_location = $("#setting-location .mm-counter");
+                $("#locations").find( "li span" ).click(function() {
+                  $set_location.text( $(this).text() );
+                  api.openPanel( $settings );
+                });
+
               });
+      
             }
-        }
+        };
 
     });
 })();
